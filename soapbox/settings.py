d8daef4110adcba846470da3a34bcba1d58997eb
Ian Buttimer <ibuttimer@yahoo.com>
2022-11-12 13:50:00 +0000

    maint: Standardise location of static files

--- soapbox/settings.py
+++ soapbox/settings.py
@@ -316,9 +316,10 @@ USE_TZ = True
 # URL to use when referring to static files located in STATIC_ROOT
 STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#staticfiles-storage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' \
-    if DEVELOPMENT else \
-    'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    # \
+    # if DEVELOPMENT else \
+    # 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-STATICFILES_DIRS
 # Additional locations the staticfiles app will traverse for collectstatic
 STATICFILES_DIRS = [
